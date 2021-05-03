9a33faef8cf4ea9c1ad2ed88a2eba7b7f72449f6
KRom-i <emailroma@mail.ru>
2021-05-04 00:51:30 +0300

    addFileTest

--- java_03/src/main/java/AddFileTestGitPush.java
+++ java_03/src/main/java/AddFileTestGitPush.java
@@ -3,6 +3,9 @@ public class AddFileTestGitPush {
     public static void main(String[] args) {
 
         System.out.println("AddFileTestGitPush");
+        System.out.println("AddFileTestGitPush");
+        System.out.println("AddFileTestGitPush");
+        System.out.println("AddFileTestGitPush");
     }
 
 }
